17567d98b8b9a6cb7f82537f59fd67cad187310d
RedGoopty <hyypaoiva@gmail.com>
2023-01-07 10:28:28 +0200

    configured react-router & made the Nav component

--- webage/src/App.js
+++ webage/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Nav } from "./components/Nav";
+import About from "./components/About"
+import Home from "./components/Home"
+import Music from "./components/Music"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav /> // render the Nav component
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/music" component={Music} />
+    </Router>
   );
 }
 
